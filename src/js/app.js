5885ca0ded9d9695173a067e3f640de0d5ac2ada
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-07-16 03:40:36 +0000

    operador ternario  ejercicio complete

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1> 
+            ${variables.name == null ? "lucy" : variables.name} 
+            ${variables.lastName == null ? "Boilett" : variables.lastName}
+          </h1>
+          <h2>
+            ${variables.role == null ? "Select Role" : variables.role}
+          </h2>
+          <h3>
+            ${variables.city == null ? "Select City" : variables.city},
+            ${variables.country == null ? " Country" : variables.country}
+          </h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "GustavoL43680" : variables.twitter
+            }" target="blank"><i class="fab fa-twitter"></i></a>
+            </li>
+           <li><a href="https://github.com/${
+             variables.github == null ? "G-code7" : variables.github
+           }" target="blank"><i class="fab fa-github"></i></a>
+           </li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null ? "Gustavo" : variables.linkedin
+            }" target="blank"><i class="fab fa-linkedin"></i></a>
+            </li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "gustavo" : variables.instagram
+            }" target="blank"><i class="fab fa-instagram"></i></a>
+            </li>
           </ul>
         </div>
     `;
